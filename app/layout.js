5bb24a3efd583d1d2002eff54bfda12cfbdd8e7b
KIMSOOHWAN <ksooh95@gmail.com>
2024-05-30 16:26:36 +0900

    fix : vercel 배포용 session 삭제&수정

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,5 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import TopMenu from './topMenu';
-import TopMenu2 from './topMenu2';
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
-import { getServerSession } from 'next-auth';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,12 +9,9 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-    let session = await getServerSession(authOptions);
-    console.log('session :', session);
     return (
         <html lang="en">
             <body>
-                {session == null ? <TopMenu session={session} /> : <TopMenu2 session={session} />}
                 <div className="bg">
                     <img src="/img/bg.jpeg" alt="메인 배경 이미지" />
                 </div>
